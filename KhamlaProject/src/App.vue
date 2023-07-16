cc7798dbaf54ff7480c343dfe2bde0dc656e04bc
khamlalao <oppo93897429@gmail.com>
2023-07-16 14:42:38 +0700

    project init

---
+++ KhamlaProject/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="d-flex flex-row-reverse">
+  <div class="p-2">ສະບາຍດີ</div>
+  <div lang = "la" class ="p-2">ສະບາຍດີ</div>
+  <div class="p-2">Flex item 3</div>
+</div>
+
+</template>
+ 
+<script>
+export default {
+
+}
+</script>
+
+
